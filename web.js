684431859dfae441b7d7fe8589cfe335bcef01f6
mohanjune <mohanjune@gmail.com>
2013-07-21 22:09:00 +0000

    Updated index.html

--- web.js
+++ web.js
@@ -1,14 +1,14 @@
 var express = require('express');
 var fs = require('fs');
 
-var app = express.createServer(express.logger());
+var app = express(express.logger());
 
 app.get('/', function(request, response) {
   var buf = new Buffer(fs.readFileSync('index.html'), 'utf-8');
   response.send(buf.toString());
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
