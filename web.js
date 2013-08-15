dd88db53b17f2f3c2048d77ac926c0c99a01fa58
mohanjune <mohanjune@gmail.com>
2013-08-15 20:29:36 +0000

    Changed web.js

--- web.js
+++ web.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var fs = require('fs');
 
-var app = express(express.logger());
+var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
   var buf = new Buffer(fs.readFileSync('index.html'), 'utf-8');
